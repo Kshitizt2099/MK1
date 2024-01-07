dceeb72ad934d7c019d3959b08594c154593dead
Kshitizt2099 <109498859+Kshitizt2099@users.noreply.github.com>
2024-01-08 00:09:09 +0530

    Update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -1,11 +1,11 @@
 import { Controller } from "../Models/UserModel.js";
 import { user } from "../Models/UserSchema.js";
 import mongoose from "mongoose";
-
+const UserModel=mongoose.model("ShUsers",user);
 export class UsersController{
     async home(req,res)
     {
-        
+   
        res.render('Home')
        
         
@@ -15,7 +15,15 @@ export class UsersController{
     {
         Controller.signUp(req.body);
         
-    
+        
         res.render('Home')            
+    }
+
+    async AllUsers(req,res)
+    {
+        let data=await UserModel.find({});
+        console.log(data)
+        return res.render('AllNames',{data})
+
     }
 }
